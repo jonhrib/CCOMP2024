5ca9caf330cbc64106b2d70528a049d888af4ea2
João Vitor Souza Ribeiro <jonhvictor121@gmail.com>
2024-05-08 08:55:14 -0300

    atualiza atividade

--- Inteligência Artificial/1º Bimestre/Atividade3/Atributo.java
+++ Inteligência Artificial/1º Bimestre/Atividade3/Atributo.java
@@ -2,19 +2,19 @@ package Arquivos;
 
 import java.util.ArrayList;
 
-public class Atributo {
-    private ArrayList <Boolean> valores;
+public class Atributo { //classe do Atributo
+    private ArrayList <Boolean> valores; //atributo lista, que gardará os valores do Atributo
 
-    public Atributo (boolean valor){
+    public Atributo (boolean valor){ //construtor que recebe o valor diretamente, criando a lista e colocando o valor nela
         this.valores = new ArrayList<>();
         this.valores.add(valor);
     }
 
-    public void adicionarValor(boolean valor) {
+    public void adicionarValor(boolean valor) { // função que adiciona os valores diretamente na lista
         this.valores.add(valor);
     }
 
-    public ArrayList<Boolean> getValores() {
+    public ArrayList<Boolean> getValores() { //retorna os valores 
         return this.valores;
     }
 }
--- Inteligência Artificial/1º Bimestre/Atividade3/Main.java
+++ Inteligência Artificial/1º Bimestre/Atividade3/Main.java
@@ -5,53 +5,53 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Double> IGs = new ArrayList<>();
-        List<String> NomesClasse = new ArrayList<>();
+        List<Double> IGs = new ArrayList<>(); //lista dos ganhos de informação
+        List<String> NomesClasse = new ArrayList<>(); //lista dos nomes das classes (outlook, temperature, humidity e windy)
 
         //Atributos de outlook
-        Atributo sunny = new Atributo (true);
-        sunny.adicionarValor(true);
-        sunny.adicionarValor(false);
-        sunny.adicionarValor(false);
-        sunny.adicionarValor(false);
+        Atributo sunny = new Atributo (true); //cria um atributo sunny, que recebe true como o primeiro valor
+        sunny.adicionarValor(true); //adiciona novo valor ao sunny
+        sunny.adicionarValor(false); //adiciona novo valor ao sunny
+        sunny.adicionarValor(false); //adiciona novo valor ao sunny
+        sunny.adicionarValor(false); //adiciona novo valor ao sunny
 
-        Atributo overcast = new Atributo (true);
-        overcast.adicionarValor(true);
-        overcast.adicionarValor(true);
-        overcast.adicionarValor(true);
+        Atributo overcast = new Atributo (true); //cria um atributo overcast, que recebe true como o primeiro valor
+        overcast.adicionarValor(true); //adiciona novo valor ao overcast
+        overcast.adicionarValor(true); //adiciona novo valor ao overcast
+        overcast.adicionarValor(true); //adiciona novo valor ao overcast
 
-        Atributo rainy = new Atributo (true);
-        rainy.adicionarValor(true);
-        rainy.adicionarValor(true);
-        rainy.adicionarValor(false);
-        rainy.adicionarValor(false);
+        Atributo rainy = new Atributo (true); //cria um atributo rainy, que recebe true como o primeiro valor
+        rainy.adicionarValor(true); //adiciona novo valor ao rainy
+        rainy.adicionarValor(true); //adiciona novo valor ao rainy
+        rainy.adicionarValor(false); //adiciona novo valor ao rainy
+        rainy.adicionarValor(false); //adiciona novo valor ao rainy
 
-        List<Atributo> outlook = new ArrayList<>();
-        outlook.add(sunny);
-        outlook.add(overcast);
-        outlook.add(rainy);
+        List<Atributo> outlook = new ArrayList<>(); //cria uma lista outlook, que portará todos os atributos que fazem parte dele
+        outlook.add(sunny); //adiciona sunny em outlook
+        outlook.add(overcast); //adiciona overcast em outlook
+        outlook.add(rainy); //adiciona rainy em outlook
         
         //---------------------------------
 
         //Atributos de temperature
-        Atributo hot = new Atributo (true);
+        Atributo hot = new Atributo (true); //cria um atributo hot, que recebe true como o primeiro valor
         hot.adicionarValor(true);
         hot.adicionarValor(false);
         hot.adicionarValor(false);
 
-        Atributo mild = new Atributo (true);
+        Atributo mild = new Atributo (true); //cria um atributo mild, que recebe true como o primeiro valor
         mild.adicionarValor(true);
         mild.adicionarValor(true);
         mild.adicionarValor(true);
         mild.adicionarValor(false);
         mild.adicionarValor(false);
 
-        Atributo cool = new Atributo (true);
+        Atributo cool = new Atributo (true); //cria um atributo cool, que recebe true como o primeiro valor
         cool.adicionarValor(true);
         cool.adicionarValor(true);
         cool.adicionarValor(false);
 
-        List<Atributo> temperature = new ArrayList<>();
+        List<Atributo> temperature = new ArrayList<>(); //cria uma lista temperature, que portará todos os atributos que fazem parte dele
         temperature.add(hot);
         temperature.add(mild);
         temperature.add(cool);
@@ -59,7 +59,7 @@ public class Main {
         //---------------------------------
 
         //Atributos de humidity
-        Atributo high = new Atributo (true);
+        Atributo high = new Atributo (true); //cria um atributo high, que recebe true como o primeiro valor
         high.adicionarValor(true);
         high.adicionarValor(true);
         high.adicionarValor(false);
@@ -67,7 +67,7 @@ public class Main {
         high.adicionarValor(false);
         high.adicionarValor(false);
 
-        Atributo normal = new Atributo (true);
+        Atributo normal = new Atributo (true); //cria um atributo normal, que recebe true como o primeiro valor
         normal.adicionarValor(true);
         normal.adicionarValor(true);
         normal.adicionarValor(true);
@@ -75,14 +75,14 @@ public class Main {
         normal.adicionarValor(true);
         normal.adicionarValor(false);
 
-        List<Atributo> humidity = new ArrayList<>();
+        List<Atributo> humidity = new ArrayList<>(); //cria uma lista humidity, que portará todos os atributos que fazem parte dele
         humidity.add(high);
         humidity.add(normal);
 
         //---------------------------------
 
         //Atributos de windy
-        Atributo falsee = new Atributo (true);
+        Atributo falsee = new Atributo (true); //cria um atributo falsee, que recebe true como o primeiro valor
         falsee.adicionarValor(true);
         falsee.adicionarValor(true);
         falsee.adicionarValor(true);
@@ -91,14 +91,14 @@ public class Main {
         falsee.adicionarValor(false);
         falsee.adicionarValor(false);
 
-        Atributo truee = new Atributo (true);
+        Atributo truee = new Atributo (true); //cria um atributo truee, que recebe true como o primeiro valor
         truee.adicionarValor(true);
         truee.adicionarValor(true);
         truee.adicionarValor(false);
         truee.adicionarValor(false);
         truee.adicionarValor(false);
 
-        List<Atributo> windy = new ArrayList<>();
+        List<Atributo> windy = new ArrayList<>(); //cria uma lista windy, que portará todos os atributos que fazem parte dele
         windy.add(falsee);
         windy.add(truee);
 
@@ -106,16 +106,18 @@ public class Main {
 
         //impressões
         System.out.println("-------------------------------------------------");
-        for (Atributo atributo : outlook) {
-            ArrayList<Boolean> valores = atributo.getValores();
+        System.out.println("\u001B[\u001B[33m Atividade de IA - Entropia e Ganho de Informação\u001B[0m");
+        System.out.println("-------------------------------------------------");
+        for (Atributo atributo : outlook) { //percorre todos os atributos de outlook
+            ArrayList<Boolean> valores = atributo.getValores(); //coloca em um ArrayList, os valores do atributo, true e false
             System.out.print("Valores O: ");
-            for (Boolean valor : valores) {
+            for (Boolean valor : valores) { //imprime os valores
                 System.out.print(valor + " ");
             }
             System.out.println();
         }
         System.out.println("-------------------------------------------------");
-        for (Atributo atributo : temperature) {
+        for (Atributo atributo : temperature) { //percorre todos os atributos de temperature
             ArrayList<Boolean> valores = atributo.getValores();
             System.out.print("Valores T: ");
             for (Boolean valor : valores) {
@@ -124,7 +126,7 @@ public class Main {
             System.out.println();
         }
         System.out.println("-------------------------------------------------");
-        for (Atributo atributo : humidity) {
+        for (Atributo atributo : humidity) { //percorre todos os atributos de humidity
             ArrayList<Boolean> valores = atributo.getValores();
             System.out.print("Valores H: ");
             for (Boolean valor : valores) {
@@ -133,7 +135,7 @@ public class Main {
             System.out.println();
         }
         System.out.println("-------------------------------------------------");
-        for (Atributo atributo : windy) {
+        for (Atributo atributo : windy) { //percorre todos os atributos de windy
             ArrayList<Boolean> valores = atributo.getValores();
             System.out.print("Valores W: ");
             for (Boolean valor : valores) {
@@ -149,138 +151,137 @@ public class Main {
         int yes = 0;
         int no = 0;
 
-        for (Atributo atributo : outlook) {
-            ArrayList<Boolean> valores = atributo.getValores();
+        for (Atributo atributo : outlook) { //percorre todos os atributos de outlook
+            ArrayList<Boolean> valores = atributo.getValores(); //armazena em uma lista os valores do atributo, false ou true
             for (Boolean valor : valores) {
-                if (valor) {
+                if (valor) { //se o valor for true (yes)
                     yes++;
-                } else {
+                } else { //se o valor for false (no)
                     no++;
                 }
             }
         }
 
-        // Chamar a função entropiaGeral para calcular a entropia geral, passando o total de valores, a quantidade de yes e a qtd de no
-        int total = yes + no;
-        double eG = entropiaGeral(total, yes, no);
+        int total = yes + no; //a quantidade total de valores no atributo
+        double eG = entropiaGeral(total, yes, no); // chama a função que calcula a entropia geral, passando o total de valores, a quantidade de yes e a qtd de no
         System.out.println("Entropia Geral: " + eG);
 
         System.out.println();
 
         //impressões
-        double eO = entropia(outlook);
+        double eO = entropia(outlook); //chama a função que calcula a entropia de outlook
         System.out.println("Entropia Outlook: " + eO);
-        double gO = IG(eO,eG);
+        double gO = IG(eO,eG); //chama a função que calcula o ganho de informação de outlook
         System.out.println("Ganho de Informação Outlook: " + gO);
 
         System.out.println();
 
-        double eT = entropia(temperature);
+        double eT = entropia(temperature); //chama a função que calcula a entropia de temperature
         System.out.println("Entropia Temperature: " + eT);
-        double gT = IG(eT,eG);
+        double gT = IG(eT,eG); //chama a função que calcula o ganho de informação de temperature
         System.out.println("Ganho de Informação Temperature: " + gT);
 
         System.out.println();
 
-        double eH = entropia(humidity);
+        double eH = entropia(humidity); //chama a função que calcula a entropia de humidity
         System.out.println("Entropia Humidity: " + eH);
-        double gH = IG(eH,eG);
+        double gH = IG(eH,eG); //chama a função que calcula o ganho de informação de humidity
         System.out.println("Ganho de Informação Humidity: " + gH);
 
         System.out.println();
 
-        double eW = entropia(windy);
+        double eW = entropia(windy); //chama a função que calcula a entropia de windy
         System.out.println("Entropia Windy: " + eW);
-        double gW = IG(eW,eG);
+        double gW = IG(eW,eG); //chama a função que calcula o ganho de informação de windy
         System.out.println("Ganho de Informação Windy: " + gW);
 
-        NomesClasse.add("outlook");
-        IGs.add(gO);
-        NomesClasse.add("temperature");
-        IGs.add(gT);
-        NomesClasse.add("humidity");
-        IGs.add(gH);
-        NomesClasse.add("windy");
-        IGs.add(gW);
+        NomesClasse.add("outlook"); // adiciona outlook a lista de nomes de classes
+        IGs.add(gO); //adiciona o ganho de informação de outlook na lista de ganhos da informação, na mesma posição do nome outlook
+        NomesClasse.add("temperature"); // adiciona temperature a lista de nomes de classes
+        IGs.add(gT); //adiciona o ganho de informação de temperature na lista de ganhos da informação, na mesma posição do nome temperature
+        NomesClasse.add("humidity"); // adiciona humidity a lista de nomes de classes
+        IGs.add(gH); //adiciona o ganho de informação de humidity na lista de ganhos da informação, na mesma posição do nome humidity
+        NomesClasse.add("windy"); // adiciona windy a lista de nomes de classes
+        IGs.add(gW); //adiciona o ganho de informação de windy na lista de ganhos da informação, na mesma posição do nome windy
 
-        double maiorg = Double.MIN_VALUE;
-        int indmaior = -1;
+        double maiorg = Double.MIN_VALUE; //maiorg recebe o menor valor double
+        int indmaior = -1; //o índice inicia em -1
 
-        for (int i = 0; i < IGs.size(); i++) {
-            double ganho = IGs.get(i);
-            if (ganho > maiorg) {
-                maiorg = ganho;
-                indmaior = i;
+        for (int i = 0; i < IGs.size(); i++) { //percorre os ganhos de informação da lista
+            double ganho = IGs.get(i); //ganho recebe cada ig
+            if (ganho > maiorg) { //se o ganho atual for maior que o maior ganho até agora
+                maiorg = ganho; //armazena-se o maior ganho
+                indmaior = i; //armazenamos o índice do maior ganho
             }
         }
         System.out.println("-------------------------------------------------");
-        System.out.println("A raiz escolhida deve ser: " + NomesClasse.get(indmaior));
+        System.out.println("A raiz escolhida deve ser: \u001B[4m\u001B[35m" + NomesClasse.get(indmaior) + "\u001B[0m"); //imprime o nome na posição do maior ganho
         System.out.println("-------------------------------------------------");
 
     }
 
-    static double entropia(List<Atributo> atributos) {
-        double entropiatotal = 0;
+    static double entropia(List<Atributo> atributos) { //função que calcula a entropia de um conjunto de atributos
+        double entropiatotal = 0; //entropia total do conjunto
     
-        int atributostotais = 0;
+        int valorestotais = 0; //quantidade de valores totais, de todos os atributos
         for (Atributo atributo : atributos) { //soma todos os valores dos atributos, aqui 14
-            atributostotais += atributo.getValores().size();
+            valorestotais += atributo.getValores().size();
         }
     
         // calcula a entropia para cada atributo e soma elas
-        for (Atributo atributo : atributos) {
-            ArrayList<Boolean> valores = atributo.getValores();
-            int total = valores.size();
-            int totalTrue = 0;
+        for (Atributo atributo : atributos) { //percorre todos os atributos
+            ArrayList<Boolean> valores = atributo.getValores(); //uma lista recebe todos os valores do atributo
+            int total = valores.size(); //total = a quantidade de valores neste atributo
+            int totalTrue = 0; //quantidade de valores yes
     
             // conta o número de yes
-            for (Boolean valor : valores) {
-                if (valor) {
+            for (Boolean valor : valores) { //percorre os valores do atributo 
+                if (valor) { //se o valor é verdadeiro, ou seja, yes
                     totalTrue++;
                 }
             }
     
             // calculaa proporção de yes ou no
-            double pTrue = (double) totalTrue / total;
-            double pFalse = 1.0 - pTrue;
+            double pTrue = (double) totalTrue / total; //proporção de yes
+            double pFalse = 1.0 - pTrue; //proporção de no
     
             // calcula a entropia mesmo
-            double entropy = 0;
+            double e = 0;
             if (pTrue != 0) { //se a proporção de yes for diferente de 0, se for igual não faz nada, logo, continua zero aqui
-                entropy -= pTrue * (Math.log(pTrue) / Math.log(2)); //tem o menos no começo da fórmula
+                e -= pTrue * (Math.log(pTrue) / Math.log(2)); //tem o menos no começo da fórmula
             }
             if (pFalse != 0) { //se a proporção de no for diferente de 0, se for igual não faz nada, logo, continua zero aqui
-                entropy -= pFalse * (Math.log(pFalse) / Math.log(2)); //tem o menos no meio da fórmula, antes desse bloco
+                e -= pFalse * (Math.log(pFalse) / Math.log(2)); //tem o menos no meio da fórmula, antes desse bloco
             }
     
             // soma a entropia de cada atributo multiplicado pelo total de valores dele dividido pela qauntidade total de valores
-            entropiatotal += ((double) total / atributostotais) * entropy;
+            entropiatotal += ((double) total / valorestotais) * e; //a entropiatotal do conjunto (outlook) recebe as entropias dos atributos, somando-se
         }
     
-        return entropiatotal;
+        return entropiatotal; //retorna a entropia da classe
     }
 
-    static double entropiaGeral(int total, int yes, int no) {
+    static double entropiaGeral(int total, int yes, int no) { //função que calcula a entropia geral de todas as classes
         // calculando a proporção de yes e no
-        double pTrue = (double) yes / total;
-        double pFalse = (double) no / total;
+        double pTrue = (double) yes / total; //calcula a proporção de yes
+        double pFalse = (double) no / total; //calcula a proporção de no
     
         // calculando a entropia geral
         double entropia = 0;
         if (pTrue != 0) {
-            entropia -= pTrue * (Math.log(pTrue) / Math.log(2));
+            entropia -= pTrue * (Math.log(pTrue) / Math.log(2)); //tem o menos no começo da fórmula
         }
         if (pFalse != 0) {
-            entropia -= pFalse * (Math.log(pFalse) / Math.log(2));
+            entropia -= pFalse * (Math.log(pFalse) / Math.log(2)); //tem o menos no meio da fórmula, antes desse bloco
         }
     
-        return entropia;
+        return entropia; //retorna a entropia da totalidade
     }
     
-    static double IG(double entropia, double entropiaGeral) {
+    static double IG(double entropia, double entropiaGeral) { //calcula o ganho de cada conjunto
         //double ganho = 0.940 - entropia;
-        double ganho = entropiaGeral - entropia;
-        return ganho;
+        double ganho = entropiaGeral - entropia; // o ganho se baseia na entropiageral menos a entropia específica
+        return ganho; //devolvemos o ganho para a chamada
     }
     
 }
